e6d7d58907249fa4154e9c13c318150fe6bff4cd
Cédric Legendre <albundy1385@gmail.com>
2015-03-12 18:54:57 +0100

    -Get My Mozaic

---
+++ ImageMosaic.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ImageMosaic;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+
+    /// <summary> 
+    /// Interaction logic for MainWindow.xaml 
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // For this example, values are hard-coded to a mosaic of 8x8 tiles. 
+
+            button1.Click += button1_Click;
+
+        }
+
+        private void button1_Click( object sender, RoutedEventArgs e )
+        {
+            MosaicGenerator generator = new MosaicGenerator();
+
+            int take = SourceImagesCount();
+
+            string imageToMash = @"C:\Users\cedri_000\Pictures\iCloud Photos\PhotoStream2\" + "_IMG_2658.JPG";
+            string srcImageDirectory = @"C:\Users\cedri_000\Pictures\iCloud Photos";
+
+            var result = generator.Generate( imageToMash, srcImageDirectory, take );
+            SetImageSource( result.Image );
+        }
+
+        private int SourceImagesCount()
+        {
+            int take = 100000;
+            string content = ((ComboBoxItem)combo1.SelectedItem).Content.ToString();
+            if( content != "All" ) take = Int32.Parse( ((ComboBoxItem)combo1.SelectedItem).Content.ToString() );
+            return take;
+        }
+
+        private void SetImageSource( System.Drawing.Image image )
+        {
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+
+            MemoryStream ms = new MemoryStream();
+            image.Save( ms, ImageFormat.Bmp );
+            ms.Seek( 0, SeekOrigin.Begin );
+            bi.StreamSource = ms;
+
+            bi.EndInit();
+            image1.Source = bi;
+        }
+
+        private void ComboBox_SelectionChanged( object sender, SelectionChangedEventArgs e )
+        {
+
+        }
+    }
+}
